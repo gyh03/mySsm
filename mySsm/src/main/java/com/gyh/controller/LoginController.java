2901155110d1af5eb5f06833a4d8e296929f2f35
螺丝钉 <32859363+gyh03@users.noreply.github.com>
2017-11-15 20:45:56 +0800

    配置

--- mySsm/src/main/java/com/gyh/controller/LoginController.java
+++ mySsm/src/main/java/com/gyh/controller/LoginController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,15 +20,23 @@ import com.gyh.common.inteceptor.SkipAuthCheck;
 import com.gyh.service.UserService;
 import com.gyh.utils.encrypt.MD5Util;
 
+import redis.clients.jedis.JedisCluster;
+
 @Controller
 @RequestMapping("/loginController")
 public class LoginController {
 	private Logger log=Logger.getLogger(LoginController.class);
 	
 	@Autowired
-	UserService userService;
+	public UserService userService;
 	/*@Autowired
-	RedisClientTemplate redisClient;*/
+	public RedisTemplate<String,String> redisSingle;*/
+	@Autowired
+	public JedisCluster redisCluster;
+	
+	
+	@Value("#{prop['jdbc.driver']}")
+	public String driver;
 	
 	//登录--支持手机和用户名登录
 	@SkipAuthCheck //排除拦截
@@ -47,6 +56,10 @@ public class LoginController {
 		}
 		map.put("loginname", loginname);
 		map.put("password", MD5Util.string2MD5(password));
+//		System.out.println(redisSingle+"<<<<<<<<<<<<");
+//		redisSingle.opsForValue().set("single", "jajaja",1200,TimeUnit.SECONDS);
+//		redisCluster.set("cluster", "hahaha");
+		String a =redisCluster.get("cluster");
 		User user=userService.login(map);	
 		
 		if(user!=null){
@@ -64,6 +77,7 @@ public class LoginController {
 			result.put("msg", "fail");
 			mv.addObject("result", result);
 		}
+		result.put("msg", driver+"<>"+a);
 		return result;
 	}
 
