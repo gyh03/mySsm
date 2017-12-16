deca0013ac911e650952738f34bcf53dd58e5a70
螺丝钉 <32859363+gyh03@users.noreply.github.com>
2017-12-16 22:44:46 +0800

    登录成功将用户信息存入redis

--- mySsm/src/main/java/com/gyh/controller/LoginController.java
+++ mySsm/src/main/java/com/gyh/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.gyh.controller;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gyh.utils.IDGenerator;
+import com.gyh.utils.json.JacksonUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,17 +33,17 @@ public class LoginController {
 	
 	@Autowired
 	public UserService userService;
-	/*@Autowired
+
+
+	@Autowired
 	public RedisTemplate<String,String> redisSingle;
-	//		System.out.println(redisSingle+"<<<<<<<<<<<<");
-//		redisSingle.opsForValue().set("single", "jajaja",1200,TimeUnit.SECONDS);
-//		redisCluster.set("cluster", "hahaha");
-//		String a =redisCluster.get("cluster");
-	*/
+
+
 	@Autowired(required=false)
 	public JedisCluster redisCluster;
-	
-	
+	/*redisCluster.set("cluster", "hahaha");
+	String a =redisCluster.get("cluster");*/
+
 	@Value("#{prop['jdbc.driver']}")
 	public String driver;
 	
@@ -65,20 +70,20 @@ public class LoginController {
 			user = userService.queryUserByUserName(userName);
 			if(user!=null){
 				if(user.getPassword().equals(MD5Util.string2MD5(password))){
+					String userid="loginUserId_"+user.getId()+"";
+					String uuid= IDGenerator.getUUID();
+
+					System.out.println(redisSingle+"<<<<<<<<<<<<");
+					redisSingle.opsForValue().set(userid, JacksonUtils.toJson(user),1200, TimeUnit.SECONDS);
+					redisSingle.opsForValue().set(uuid, userid,1200, TimeUnit.SECONDS);
+
+					result.setData(uuid);
+					result.setMsg("success");
+					result.setOther(user);
 					result.setSuccess(true);
-					result.setData(user);
 				}else{
 					result.setMsg("密码错误");
 				}
-				
-				/*String userid=user.getId()+"";
-				String uuid=UUIDGenerator.generate(userid);
-				redisClient.set(userid, uuid);
-				redisClient.set(uuid, userid);
-				
-				result.put("user", user);
-				result.put("msg", "success");
-				result.put("uuid", redisClient.get(userid));*/
 				
 			}else{
 				result.setMsg("用户不存在");
