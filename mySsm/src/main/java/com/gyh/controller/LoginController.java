ebbd28c855257d0ec2e5e24061ee7d9fe2d4aad7
螺丝钉 <32859363+gyh03@users.noreply.github.com>
2017-12-21 22:17:37 +0800

    redis缓存在线用户

--- mySsm/src/main/java/com/gyh/controller/LoginController.java
+++ mySsm/src/main/java/com/gyh/controller/LoginController.java
@@ -1,30 +1,29 @@
 package com.gyh.controller;
 
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.gyh.bean.TUser;
+import com.gyh.common.inteceptor.SkipAuthCheck;
+import com.gyh.common.pojo.MessageResult;
+import com.gyh.service.UserService;
 import com.gyh.utils.IDGenerator;
+import com.gyh.utils.encrypt.MD5Util;
 import com.gyh.utils.json.JacksonUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.gyh.bean.TUser;
-import com.gyh.common.inteceptor.SkipAuthCheck;
-import com.gyh.common.pojo.MessageResult;
-import com.gyh.service.UserService;
-import com.gyh.utils.encrypt.MD5Util;
-
 import redis.clients.jedis.JedisCluster;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/loginController")
@@ -32,26 +31,28 @@ public class LoginController {
 	private Logger log=Logger.getLogger(LoginController.class);
 	
 	@Autowired
-	public UserService userService;
+	private UserService userService;
 
 
-	@Autowired
-	public RedisTemplate<String,String> redisSingle;
+	@Autowired(required = false)
+	private RedisTemplate<String,String> redisSingle;
 
 
-	@Autowired(required=false)
-	public JedisCluster redisCluster;
+	@Autowired(required = false)
+	private JedisCluster redisCluster;
 	/*redisCluster.set("cluster", "hahaha");
 	String a =redisCluster.get("cluster");*/
 
 	@Value("#{prop['jdbc.driver']}")
-	public String driver;
-	
+	private String driver;
+
+	private final String OnlineUsers = "ONLINEUSERS";
+
 	//登录--支持手机和用户名登录
 	@SkipAuthCheck 
 	@ResponseBody
-	@RequestMapping("/dologin")
-	public Object dologin(HttpServletRequest request,HttpServletResponse response,String userName,String password){
+	@RequestMapping(value = "/dologin",method = RequestMethod.POST)
+	public MessageResult dologin(HttpServletRequest request,HttpServletResponse response,String userName,String password){
 		MessageResult result = new MessageResult();
 		result.setSuccess(false);
 		if(StringUtils.isEmpty(userName)){
@@ -70,14 +71,20 @@ public class LoginController {
 			user = userService.queryUserByUserName(userName);
 			if(user!=null){
 				if(user.getPassword().equals(MD5Util.string2MD5(password))){
-					String userid="loginUserId_"+user.getId()+"";
-					String uuid= IDGenerator.getUUID();
+					String loginUserID="loginUserId_"+user.getId()+"";
+					String userToken= IDGenerator.getUUID();
 
 					System.out.println(redisSingle+"<<<<<<<<<<<<");
-					redisSingle.opsForValue().set(userid, JacksonUtils.toJson(user),1200, TimeUnit.SECONDS);
-					redisSingle.opsForValue().set(uuid, userid,1200, TimeUnit.SECONDS);
+					/*redisSingle.opsForValue().set(userToken, JacksonUtils.toJson(user),1200, TimeUnit.SECONDS);
+					redisSingle.opsForValue().set(loginUserID, userToken,1200, TimeUnit.SECONDS);*/
+					redisCluster.set(userToken, JacksonUtils.toJson(user));
+					//设置过期时间
+					redisCluster.expire(userToken,1200);
+					redisCluster.set(loginUserID,userToken);
+					//记录在线用户
+					redisCluster.rpush(OnlineUsers,loginUserID);
 
-					result.setData(uuid);
+					result.setData(userToken);
 					result.setMsg("success");
 					result.setOther(user);
 					result.setSuccess(true);
@@ -95,4 +102,37 @@ public class LoginController {
 		return result;
 	}
 
+	@ResponseBody
+	@RequestMapping("/onlineUsers")
+	public MessageResult onlineUsers(){
+		MessageResult result = new MessageResult();
+
+		List<String> onlineUserIds = redisCluster.lrange(OnlineUsers,0,-1);
+		List<String> offlineUserIds = new ArrayList<String>();
+		String userId = null;
+		String useTroken = null;
+		String userJson = null;
+		Map<String,String> userMap = new HashMap<>();
+		List<TUser> onlineUsers = new ArrayList<>();
+		for (int i = 0; i < onlineUserIds.size(); i++) {
+			userId = onlineUserIds.get(i);
+			useTroken = redisCluster.get(userId);
+			userJson = redisCluster.get(useTroken);
+			if(StringUtils.isBlank(useTroken) && StringUtils.isBlank(userJson)){
+				offlineUserIds.add(userId);
+			}else{
+				userMap.put(userId,useTroken);
+				onlineUsers.add(JacksonUtils.fromJson(userJson,TUser.class));
+			}
+		}
+		onlineUserIds.removeAll(offlineUserIds);
+		redisCluster.del(OnlineUsers);
+		for (int i = 0; i <onlineUserIds.size() ; i++) {
+			userId = onlineUserIds.get(i);
+			redisCluster.rpush(userId,userMap.get(userId));
+		}
+		result.setSuccess(true);
+		result.setData(onlineUsers);
+		return result;
+	}
 }
